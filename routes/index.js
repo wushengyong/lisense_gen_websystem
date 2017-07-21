ecfb042bd42d34d2e30b3e2e986b1d5969f347c1
wushengyong <375228075@qq.com>
2017-07-21 17:02:31 +0800

    1. 提交基本备份。

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var conv = require('iconv-lite');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
